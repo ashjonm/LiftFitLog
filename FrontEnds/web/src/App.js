e80ea5fe68c72fd5263d615c866415b2698bdd3c
Jonathan Ash <jonathan.m.ash@outlook.com>
2021-01-18 10:31:29 -0500

    NavBar ruff style

--- FrontEnds/web/src/App.js
+++ FrontEnds/web/src/App.js
@@ -1,16 +1,12 @@
-// import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/nav-bar/nav-bar"
+import "./App.css";
 
 function App() {
   const today = new Date();
 
   return (
     <div className="App">
-      <nav className="Nav-Bar">
-        <div className="Logo" ><h4>LiftFitLog</h4></div>
-        <nav className="Nav-Button" >Journal</nav>
-        <div className="Login" /> 
-      </nav>
+      <NavBar />
       <div className="Page">
         <header className="Header">
           <h1 className="PageName">Journal</h1>
---
+++ FrontEnds/web/src/components/nav-bar/nav-bar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "./nav-bar.css";
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div className="nav-bar">
+                <div className="app-logo" />
+                <div className="nav-button" >Log</div>
+                <button className="login">Login / Logout</button>
+            </div>
+        );
+    };
+};
+
+export default NavBar;
