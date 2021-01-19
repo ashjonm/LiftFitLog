78ad44079c72f6251feac075b20e59f3ef71b4a3
Jonathan Ash <jonathan.m.ash@outlook.com>
2021-01-18 20:17:33 -0500

    Daily info portion completed

--- FrontEnds/web/src/components/journal/day.js
+++ FrontEnds/web/src/components/journal/day.js
@@ -4,17 +4,22 @@ import "./day.css";
 
 class Day extends React.Component {
     render() {
+        const day = {
+            date: new Date("01/13/2021"),
+            weight: 287.3,
+            notes: "Had a good day at the gym."
+        };
+
         return (
             <div className="day">
-                <div className="day-info"></div>
+                <div className="day-info">
+                    <div className="day-date">{day.date.toLocaleDateString("en-us", {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'})}</div>
+                    <div className="day-weight">Weight: {day.weight} lbs</div>
+                    <div className="day-notes">{day.notes || "Notes:"}</div>
+                </div>
                 <div className="day-entries"></div>
         
             </div>   
-            //     <div className="Day">
-            //     </div>
-            //     <hr/>
-            //     <div className="Day">
-            //     </div> 
         );
     };
 };
