22103d6d0fa970d7145ebe8d311a8f3496c1de92
Jonathan Ash <jonathan.m.ash@outlook.com>
2021-02-17 20:49:23 -0500

    pull days into stae and set on initize with useEffect

--- FrontEnds/web/src/components/journal/journal.js
+++ FrontEnds/web/src/components/journal/journal.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import Day from "./day"
 import "./journal.css";
 
 export default function Journal() {
-    let day = {
-        date: new Date("01/13/2021"),
-        weight: 287.3,
-        notes: "Chest & Arm Day"
-    };
+    const [days, setDays] = useState([]);
 
-    let days = [
-            {...day, id:1, date:new Date("01/11/2021")},
-            {...day, id:2, date:new Date("01/12/2021")}, 
-            {...day, id:3, date:new Date("01/13/2021")}, 
-            {...day, id:4, date:new Date("01/14/2021")}
-    ];
+    useEffect(() => {
+        let day = {
+            date: new Date("01/13/2021"),
+            weight: 287.3,
+            notes: "Chest & Arm Day"
+        };
+    
+        let defaultDays = [
+                {...day, id:1, date:new Date("01/11/2021")},
+                {...day, id:2, date:new Date("01/12/2021")},
+                {...day, id:3, date:new Date("01/13/2021")},
+                {...day, id:4, date:new Date("01/14/2021")}
+        ];
+
+        setDays(defaultDays);
+    },[]);
 
     return (
         <div className="journal">
             {days.map(d => <Day key={d.id} day={d}/>)}
-        </div>   
+        </div>
     );
 };
