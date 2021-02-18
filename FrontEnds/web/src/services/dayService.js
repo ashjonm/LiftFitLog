cf234cffaf09b6391138634bd1e7bddaa7d6e044
Jonathan <jonathan.m.ash@outlook.com>
2021-02-18 00:12:36 -0500

    rename day to baseDay

--- FrontEnds/web/src/services/dayService.js
+++ FrontEnds/web/src/services/dayService.js
@@ -2,15 +2,15 @@ export function getDays() {
     return localDays;
 };
 
-const day = {
+const baseDay = {
     date: new Date("01/13/2021"),
     weight: 287.3,
     notes: "Chest & Arm Day"
 };
 
 const localDays = [
-    {...day, id:1, date:new Date("01/11/2021")},
-    {...day, id:2, date:new Date("01/12/2021")},
-    {...day, id:3, date:new Date("01/13/2021")},
-    {...day, id:4, date:new Date("01/14/2021")}
+    {...baseDay, id:1, date:new Date("01/11/2021")},
+    {...baseDay, id:2, date:new Date("01/12/2021")},
+    {...baseDay, id:3, date:new Date("01/13/2021")},
+    {...baseDay, id:4, date:new Date("01/14/2021")}
 ];
